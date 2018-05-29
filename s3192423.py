a1676305aa1cb6ab77a76be21f025cd5c22e84ff
M. Popescu <s3192423@fwn-bborg-edu-74-97.fmnsedu.rug.nl>
2018-05-29 17:08:16 +0200

    added members and ingredients functionality

--- s3192423.py
+++ s3192423.py
@@ -5,23 +5,34 @@ import re
 
 nlp = spacy.load('en')
 # An example of different formulations of the same question
-    # What is the president of Japan?
+    # Who is the president of Japan?
     # Name the president of Japan
     # State the president of Japan
     # What person is the president of Japan
     # How is the president of Japan named?
 
 def print_example_queries():
-    print("1. What is the altitude of Mont Blanc?")
-    print("2. What city is the capital of Poland?")
-    print("3. Who is the CEO of Samsung?")
-    print("4. What country is Berlin the capital of?")
-    print("5. What person is the head of state of North Korea?")
-    print("6. State the surface area of Tokyo")
-    print("7. Name the president of Zimbabwe")
-    print("8. Name the capital of the Andaman and Nicobar Islands")
-    print("9. How is the capital city of Iceland named?")
-    print("10. What person is the president of Romania?")
+    print("1. What is the altitude of Mont Blanc?") # -Gabriel
+    print("2. What city is the capital of Poland?") #  -Natasha
+    print("3. Who is the CEO of Samsung?") # -Gabriel output twice
+    print("4. What country is Berlin the capital of?") #  -Natasha -Gabriel
+    print("5. What person is the head of state of North Korea?") #   -Natasha -Gabriel
+    print("6. State the surface area of Tokyo") # -Gabriel
+    print("7. Name the president of Zimbabwe") # -Gabriel
+    print("8. Name the capital of the Andaman and Nicobar Islands") #  -Natasha -Gabriel
+    print("9. How is the capital city of Iceland named?") 
+    print("10. What person is the president of Romania?")  # -Natasha -Gabriel
+        
+def drop_the_an(sentence):
+    new_sentence = ""
+    for i in range(0, len(sentence)):
+        if sentence[i] != "an":
+            pass
+        else:
+            new_sentence[i] = sentence[i]
+    return new_sentence
+		
+
 
 params = {'action':'wbsearchentities',
           'language':'en',
@@ -40,7 +51,9 @@ dictionary_units = {
 }
 
 input_parser = {
-    'president':'head of state' # Sometimes requests with president give off prime ministers / do not work.
+    'president':'head of state', # Sometimes requests with president give off prime ministers / do not work.
+		'ingredients':'has part',
+		'members':'has part'
 }
 
 print_example_queries()
@@ -51,6 +64,11 @@ for w in sys.stdin:
     url = 'https://www.wikidata.org/w/api.php'
     params['type'] = 'item'
     wordList = re.sub("[^\w]", " ",  w).split()
+    stopWords = ['an']
+    for word in wordList:
+			  if word in stopWords:
+					  wordList.remove(word)
+
     # Format question using a dictionary to accomodate for the way
     # data is stored in wikiData. For now, only president is converted
     # to head of state, as the latter retrieves what the user actually wants.
@@ -71,11 +89,11 @@ for w in sys.stdin:
     if (last == "of?"):
         last = "of"
     for token in parse:
-        if token.dep_ == "nsubj" or token.dep_ == "attr" or token.dep_ == "dobj": 
+        if token.dep_ == "nsubj" or token.dep_ == "attr" or token.dep_ == "dobj":
             sub = []
             for d in token.subtree:
                 if d.text == "of":
-                    current_counter = current_counter + 1;
+                    current_counter = current_counter + 1
                 if d.pos_ != "ADP":
                     sub.append(d.text)
                 else:
@@ -100,6 +118,8 @@ for w in sys.stdin:
     ok = 1
     search_property = " ".join(sub)
     search_object = " ".join(obj)
+    print(search_property)
+    print(search_object)
     params['search'] = search_object
     json = requests.get(url,params).json()
     for result in json['search']:
@@ -125,6 +145,7 @@ for w in sys.stdin:
     url = 'https://query.wikidata.org/sparql'
     data = requests.get(url, params={'query': query, 'format': 'json'}).json()
     if data['results']['bindings'] == []:
+			  
         print('Unable to retrieve answer.')
     else:
       for item in data['results']['bindings']:
