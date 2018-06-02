4cd105e39ee6e5b7c666439bd2c210fba1361bf8
Mihai <mihaitu.popescu@gmail.com>
2018-06-02 13:11:51 +0200

    fixed output

--- s3192423.py
+++ s3192423.py
@@ -11,29 +11,6 @@ nlp = spacy.load('en')
     # What person is the president of Japan
     # How is the president of Japan named?
 
-def print_example_queries():
-    print("1. What is the altitude of Mont Blanc?") # -Gabriel
-    print("2. What city is the capital of Poland?") #  -Natasha
-    print("3. Who is the CEO of Samsung?") # -Gabriel output twice
-    print("4. What country is Berlin the capital of?") #  -Natasha -Gabriel
-    print("5. What person is the head of state of North Korea?") #   -Natasha -Gabriel
-    print("6. State the surface area of Tokyo") # -Gabriel
-    print("7. Name the president of Zimbabwe") # -Gabriel
-    print("8. Name the capital of the Andaman and Nicobar Islands") #  -Natasha -Gabriel
-    print("9. How is the capital city of Iceland named?") 
-    print("10. What person is the president of Romania?")  # -Natasha -Gabriel
-        
-def drop_the_an(sentence):
-    new_sentence = ""
-    for i in range(0, len(sentence)):
-        if sentence[i] != "an":
-            pass
-        else:
-            new_sentence[i] = sentence[i]
-    return new_sentence
-		
-
-
 params = {'action':'wbsearchentities',
           'language':'en',
           'format':'json'
@@ -52,87 +29,114 @@ dictionary_units = {
 
 input_parser = {
     'president':'head of state',
-		'ingredients':'has part',
-		'members':'has part'
+		'ingredient':'has part',
+		'member':'has part'
 }
 
-print_example_queries()
-
-for w in sys.stdin:
-    obj = []
-    sub = []
-    url = 'https://www.wikidata.org/w/api.php'
-    params['type'] = 'item'
-    wordList = re.sub("[^\w]", " ",  w).split()
+def print_example_queries():
+    print("1. What is the altitude of Mont Blanc?") # -Gabriel
+    print("2. What city is the capital of Poland?") #  -Natasha
+    print("3. Who is the CEO of Samsung?") # -Gabriel output twice
+    print("4. What country is Berlin the capital of?") #  -Natasha -Gabriel
+    print("5. What person is the head of state of North Korea?") #   -Natasha -Gabriel
+    print("6. State the surface area of Tokyo") # -Gabriel
+    print("7. Name the president of Zimbabwe") # -Gabriel
+    print("8. Name the capital of the Andaman and Nicobar Islands") #  -Natasha -Gabriel
+    print("9. How is the capital city of Iceland named?") 
+    print("10. What person is the president of Romania?")  # -Natasha -Gabriel
+        
+def removeUnwantedWords(wList):
     stopWords = ['an']
     for word in wordList:
 			  if word in stopWords:
 					  wordList.remove(word)
-    # Format question using a dictionary to accomodate for the way
-    # data is stored in wikiData. For now, only president is converted
-    # to head of state, as the latter retrieves what the user actually wants.
+    return wList
+
+def inputParse(wordList):
     for x in range(0, len(wordList)):
         if wordList[x] in input_parser:
             wordList[x] = input_parser.get(wordList[x])
-    stri = " ".join(wordList)
-    w = stri
-    parse = nlp(w.strip())
-    word_list = w.split()
-    first = word_list[0]
-    last = word_list[len(word_list)-1]
+    return wordList
+
+print_example_queries()
+
+def countOf(wList):
     of_counter = 0
-    current_counter = 0
     for i in range(0, len(word_list)):
         if word_list[i] == "of" or word_list[i] == "of?":
             of_counter +=1;
-    if (last == "of?"):
-        last = "of"
-    for token in parse:
-        if token.dep_ == "nsubj" or token.dep_ == "attr" or token.dep_ == "dobj" or token.dep_ == "appos":
-            sub = []
-            for d in token.subtree:
-                if d.text == "of":
-                    current_counter = current_counter + 1
-                if d.pos_ != "ADP":
+    return of_counter
+
+def extractSubject(t, p, current_counter, sub):
+    if token.dep_ == "nsubj" or token.dep_ == "attr" or token.dep_ == "dobj" or token.dep_ == "appos":
+        sub = []
+        for d in token.subtree:
+            if d.text == "of":
+                current_counter = current_counter + 1
+            if d.pos_ == 'VERB' or d.pos_ == 'NOUN':
+                sub.append(d.lemma_) 
+            elif d.pos_ != "ADP":
+                sub.append(d.text)
+            else:
+                if (d.text == last or current_counter != of_counter):
                     sub.append(d.text)
                 else:
-                    if (d.text == last or current_counter != of_counter):
-                        sub.append(d.text)
-                    else:
-                        break
-            if sub[0] == 'the':
-                del sub[0]
-        if token.dep_ == "pobj":
-            obj = []
-            for d in token.subtree:
-                obj.append(d.text)
-            if obj[0] == 'the':
-                del obj[0]
+                    break
+        if sub[0] == 'the':
+            del sub[0]
+    return sub
+
+def extractObject(t, p, obj):
+    if token.dep_ == "pobj":
+        obj = []
+        for d in token.subtree:
+            obj.append(d.text)
+        if obj[0] == 'the':
+            del obj[0]
+    return obj
+    
+def checkForProperNouns(t, p, obj):
     if not obj:
         for token in parse:
             if token.pos_ == "PROPN":
                 obj = []
                 for d in token.subtree:
                     obj.append(d.text)
+    return obj
+
+def getSearchProperty(sub):
+    # Ok is a boolean initialized to 1 to extract only one answer. Ok becomes 0
+    # after an answer has been extracted
     ok = 1
     search_property = " ".join(sub)
-    search_object = " ".join(obj)
     print(search_property)
-    print(search_object)
-    params['search'] = search_object
-    json = requests.get(url,params).json()
-    for result in json['search']:
-        if ok == 1:
-            ok = 0
-            query_object = result['id']
-    ok = 1
     params['search'] = search_property
     params['type'] = 'property';
     json = requests.get(url,params).json()
+    query_property = '@'
     for result in json['search']:
         if ok == 1:
             ok = 0
             query_property = result['id']
+    return query_property
+
+def getSearchObject(obj):
+    # Ok is a boolean initialized to 1 to extract only one answer. Ok becomes 0
+    # after an answer has been extracted
+    ok = 1
+    search_object = " ".join(obj)
+    print(search_object)
+    params['search'] = search_object
+    params['type'] = 'item'
+    json = requests.get(url,params).json()
+    query_object = '@'
+    for result in json['search']:
+        if ok == 1:
+            ok = 0
+            query_object = result['id']
+    return query_object
+
+def fireQuery(qo, qp):
     query='''
         SELECT ?valueLabel WHERE {
         wd:''' + query_object + ''' wdt:''' + query_property + ''' ?value
@@ -141,21 +145,77 @@ for w in sys.stdin:
         }
         }
         '''
+    print(query)
     url = 'https://query.wikidata.org/sparql'
     data = requests.get(url, params={'query': query, 'format': 'json'}).json()
-    if data['results']['bindings'] == []:
-			  
+    return data
+
+def printAnswer(data):
+    if not data['results']['bindings']:
         print('Unable to retrieve answer.')
     else:
-      for item in data['results']['bindings']:
-        for var in item:
-            sys.stdout.write("The " + search_property + " of " + search_object + " is ")
-            sys.stdout.write('{}'.format(item[var]['value']))
-            if search_property in dictionary_units:
-                sys.stdout.write(' ' + dictionary_units.get(search_property) + '.\n')
-            else:
-                sys.stdout.write('.\n')
-    search_property = ''
-    search_object = ''
+        for item in data['results']['bindings']:
+            for var in item:
+                sys.stdout.write('{}'.format(item[var]['value']))
+                sys.stdout.write('\n')
+
+for w in sys.stdin:
+    # Initialize lists.
+    obj = []
+    sub = []
+    data = []
+
+    # Set up initial parameters.
+    url = 'https://www.wikidata.org/w/api.php'
+    params['type'] = 'item'
+
+    # Parse the input as a list of words.
+    wordList = re.sub("[^\w]", " ",  w).split()
+    wordList = removeUnwantedWords(wordList)
+    
+    # Convert the input from a word of list to nlp strip
+    stri = " ".join(wordList)
+    w = stri
+    parse = nlp(w.strip())
+    
+    # Take the first and last word of the input for research purposes.
+    word_list = w.split()
+    first = word_list[0]
+    last = word_list[len(word_list)-1]
+
+    # Take the last word without the question mark.
+    if (last == "of?"):
+        last = "of"
+
+    # Count the number of "of" in the input.
+    of_counter = countOf(word_list)
+    current_counter = 0
+
+    # Go through each token and extract the subject and object.
+    for token in parse:
+        sub = extractSubject(token, parse, current_counter, sub)
+        obj = extractObject(token, parse, obj)
+    
+    # In case no object was found, check for proper nouns.
+    obj = checkForProperNouns(token, parse, obj)
+    sub = inputParse(sub)
+    # Extract the search property and object to feed into the query.
+    query_object = getSearchObject(obj)
+    query_property = getSearchProperty(sub)
+
+    if query_object != '@' and query_property != '@':
+        data = fireQuery(query_object, query_property)
+        if not data['results']['bindings']:
+            query_object = getSearchObject(sub)
+            query_property = getSearchProperty(obj)
+            data = fireQuery(query_object, query_property)
+    else:
+        query_object = getSearchObject(sub)
+        query_property = getSearchProperty(obj)
+        data = fireQuery(query_object, query_property)
+    printAnswer(data)
+    
+    # Reinitialize lists to empty lists.
     del obj[:]
     del sub[:]
+
